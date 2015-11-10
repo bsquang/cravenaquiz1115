e84e3f0e718dd14fb5051f23285995a64fca330d
nhtua <hoangtua.vn@gmail.com>
2015-11-10 17:06:00 +0700

    Fastclick, count user

--- js/game.js
+++ js/game.js
@@ -1,423 +1,246 @@
-var listWord = [{
-      "word": "<img src=\"img/ICON01.png\"/>",
-      "score": "50"
-  }, {
-      "word": "<img src=\"img/ICON02.png\"/>",
-      "score": "50"
-  }, {
-      "word": "<img src=\"img/ICON03.png\"/>",
-      "score": "50"
-  }, {
-      "word": "<img src=\"img/ICON04.png\"/>",
-      "score": "50"
-  }, {
-      "word": "<img src=\"img/ICON05.png\"/>",
-      "score": "50"
-  }, {
-      "word": "<img src=\"img/ICON06.png\"/>",
-      "score": "50"
-  }, {
-      "word": "<img src=\"img/ICON07.png\"/>",
-      "score": "50"
-  }, {
-      "word": "<img src=\"img/ICON08.png\"/>",
-      "score": "50"
-  }];
-  
-  var arrayBtnID = [1, 2, 3, 4, 4, 5, 6, 7, 8, 2, 1, 8, 7, 6, 5, 3];
-  var listState = {
-      "home": "1",
-      "game": "2",
-      "finish": "3",
-      "end": "4"
-  };
-  
-  var listTile = {
-      "newgame": "Game Tìm Ô Giống Nhau",
-      "ready": "Chuẩn Bị",
-      "start": "Bắt Đầu",
-      "win": "Chúc Mừng Bạn Đã Chiến Thắng",
-      "lose": "Bạn Đã Thua"
-  };
-  
-  var waitTime = 11; //10 giay cho truoc khi choi
-  var playTime = 61; //1 phut choi game
-  var btnSelected = null;
-  var btnSelected2 = null;
-  // var isSelected = false;
-  var isStart = false;
-  var isClockRun = false;
-  var countBtn = 0;
-  var mScore = 0;
-  
-  var current = 1;
-  
-  $(document).ready(function() {
-      FastClick.attach(document.body);
-      //readyGame();
-      
-      //changeState("home");      
-      eventListener();
-      //autoScale();
-  });
-  
-  function autoScale() {
-      var defaultWidth = 1024;
-      var defaultHeight = 768;
-      var thisWidth = screen.width;
-      var thisHeight = screen.height;
-      console.log(thisWidth + " / " + defaultWidth);
-      console.log(thisHeight + " / " + defaultHeight);
-      var x = 1;
-      var y = 1;
-  
-      scalePage(thisWidth / defaultWidth, thisHeight / defaultHeight);
-  }
-  
-  function scalePage(x, y) {
-      console.log(x + "," + y);
-      $('.wrapper').css({
-          '-webkit-transform': 'scale(' + x + "," + y + ')',
-          '-moz-transform': 'scale(' + x + "," + y + ')',
-          '-ms-transform': 'scale(' + x + "," + y + ')',
-          '-o-transform': 'scale(' + x + "," + y + ')',
-          'transform': 'scale(' + x + "," + y + ')'
-      });
-  
-  }
-  
-  function eventListener() {
-      $(".btn-game").bind('click', function(event) {
-          if (!$(this).hasClass("btn-touched") && isStart) {
-              $(this).addClass("btn-touched");
-              //isSelected = true;
-              if (btnSelected != null) {
-                  btnSelected2 = $(this);
-                  
-                  if (btnSelected.attr("data-id") == btnSelected2.attr("data-id")) {
-                    
-                    mScore += 50;
-                    
-                    //btnSelected.css('opacity', '0.0');
-                    //btnSelected2.css('opacity', '0.0');
-                    
-                    btnSelected = null;
-                    btnSelected2 = null;                    
-                    
-                    $("#score").text(checkScore(mScore));
-                    
-                    countBtn -= 2;
-                    if (countBtn <= 0) {
-                      
-                      stopGame();
-                    }
-  
-                  } else {
-                    
-                    mScore -= 20;
-                    $("#score").text(checkScore(mScore));
-                    
-                    setTimeout(function(){
-                      
-                      btnSelected.removeClass("btn-touched");
-                      btnSelected2.removeClass("btn-touched");
-                      btnSelected = null;
-                      btnSelected2 = null;                    
-                      
-                    },200)
-                    
-                    
-                  }
-                  
-              } else {
-                  btnSelected = $(this);
-              }
-          }
-          //console.log(event);
-  
-      });
-  
-      $(".btnStart").bind('click', function(event) {
-          changeState("play");
-      });
-      $(".btnMenu").bind('click', function(event) {
-          var btnId = $(this).attr("btn-id");
-          switch (btnId) {
-              case "help":
-                  break;
-              case "rate":
-                  break;
-              case "info":
-                  break;
-          }
-      });
-      $(".btnRePlay").bind('click', function(event) {
-          changeState("play");
-      });
-      $(".btnExit").bind('click', function(event) {
-          changeState("home");
-      });
-  }
-  
-  
-  function changeState(state) {
-      $(".page").hide();
-      switch (state) {
-          case "home":
-              $("." + state).show();
-              break;
-          case "play":
-              readyGame();
-              $("." + state).show();
-              break;
-          case "finish":
-              $("." + state).show();
-              break;
-          case "end":
-              $("." + state).show();
-              break;
-          case "help":
-              $("." + state).show();
-              break;
-      }
-  
-  }
-  
-  function gotoPage(id){
+var current = 1;
+var game = 0;
+var cate;
+var checkGameState = false;
+var checkState = false;
+var lastState = false;
+var clicked = false;
+var trueAnswer = 0;
+
+var infoState = false;
+
+var question_quantity;
+var countToOpen = 0;
+
+function checkEmail() {
+    var reg = /^([\w\.])+@([a-zA-Z0-9\-])+\.([a-zA-Z]{2,4})(\.[a-zA-Z]{2,4})?$/;
+    return (reg.test($('#email').val()));
+}
+$(document).ready(function(){
+  FastClick.attach(document.body);
+  startgame();
+  setInterval(function(){
+    countToOpen = 0;
+  }, 3000);
+})
+
+function gotoPage(id){
     
     $(".page").hide();
-    $(".page[bsq-id="+id+"]").fadeIn(300);
+    $(".page[page-id="+id+"]").fadeIn(300);
     
     current = id;
-  }
+}
   
-  function nextBUTTON() {
-    
+function nextBUTTON() {
     $(window).scrollTop(0);
-    
-    
-    if (current == 1) {
-      if(localStorage.infoEnable == '0'){
-            current = 6;      
-      }else if(localStorage.infoEnable == '1'){
-            current += 1; 
+
+    current += 1;
+    if (current == 4) {
+       checkState = true;
+       gotoGamePage(game);
+       $('.next_btn').hide();
+    }
+    if (current == 5) {
+       $('#true_answer').text('Số câu hỏi bạn trả lời đúng là: '+ trueAnswer+'/'+question_quantity);
+       if (trueAnswer == question_quantity) {
+          $('#congrat').text('Chúc mừng bạn đã chiến thắng');
+       }
+       else{
+          $('#congrat').text('Bạn chưa đạt yêu cầu !! Hãy thử lại nhé.');
+       }
+    }
+    if (current == 6) {
+      letCountUser()
+
+      if (trueAnswer == question_quantity) {
+        infoState = false;
+        lastState = true;
+        gotoPage(7);
       }
+      else{
+        infoState = false;
+        lastState = true;
+        gotoPage(7);
+      }   
+    }
+    if (current == 7) {
+        if (lastState == true) {
+        }
+        else{
+          checkInfo();
+        }
+        if (infoState == true) {
+           current = 6;
+           return;
+        }
+        setTimeout(function(){
+           window.location.reload();
+        },3000);
+    }
+    gotoPage(current);
+}
+
+function gotoGamePage(id){
+    
+    $(".question").hide();
+    $(".question[game-id="+id+"]").fadeIn(300);
+    
+    game = id;
+}
+  
+function nextQUESTION() {
+   $(window).scrollTop(0);
+   $('.rad_answer i').hide();
+   $('.next_btn1').hide();
+   
+   game += 1;
+   clicked = false;
+   if (game == question_quantity) {
+      checkGameState = true;
+   }
+   if (checkGameState == true) {
+      $('.next_btn').show();
+      $('.next_btn1').hide();
+      checkState = false;
+   }
+   gotoGamePage(game);  
+}
+
+function addQuestion(num) {
+   for(var i = 0; i < num;i++){
+      var tempDiv ='';
+      if (i == 0) {
+         tempDiv += '<div style="display:block" class="question" game-id="'+i+'">';   
+      }
+      else{
+         tempDiv += '<div class="question" game-id="'+i+'">';
+      }
+      // tempDiv +='<h1>'+title[cate].category+'</h1><br>';
+      tempDiv +='<div class="answer">';
+      tempDiv +='<h2>'+(i+1)+'. '+title[cate].question[i].ques+'</h2><br/>';
+      tempDiv +='<div class="rad_answer"><p qs-id="a" '+ (title[cate].question[i].true == 'a' ? 'istrue="true"': 'istrue="false"') +'>'+title[cate].question[i].a+'</p></div>';
+      tempDiv +='<div class="rad_answer"><p qs-id="b" '+(title[cate].question[i].true == 'b' ? 'istrue="true"': 'istrue="false"')+'>'+title[cate].question[i].b+'</p></div>';
+      tempDiv +='<div class="rad_answer"><p qs-id="c" '+(title[cate].question[i].true == 'c' ? 'istrue="true"': 'istrue="false"')+'>'+title[cate].question[i].c+'</p></div>';
+      tempDiv +='<div class="rad_answer"><p qs-id="d" '+(title[cate].question[i].true == 'd' ? 'istrue="true"': 'istrue="false"')+'>'+title[cate].question[i].d+'</p></div>';
+      tempDiv +='</div>';
+      tempDiv +='<div class="next_btn1 animated" onclick="nextQUESTION()">NEXT</div>';
+      tempDiv +='</div>';
+      
+      $('.page[page-id="4"]').append(tempDiv);
+   }
+   chooseAnswer();
+}
+function chooseAnswer() {
+   $('.rad_answer').click(function(){
+      $('.next_btn1').fadeIn(300);
+      
+      if (game == question_quantity - 1) {
+         $('.next_btn').show();
+         $('.next_btn1').hide();
+         checkState = false;
+      }
+      if (clicked == true) {
+         return;
+      }
+      else{
+         clicked = true;
+         var select = $(this).find('p').attr('istrue');
+         
+         if (select == "true") {   
+            $(this).addClass('true');
+            trueAnswer += 1;
+         }
+         else{
+            $(this).addClass('false');
+            $(this).parents('.answer').find('.rad_answer p[istrue = "true"]').parent().addClass('true');
+         }
+      }
+   })
+}
+
+function checkInfo() {
+    if ($('#name').val() == '' || $('#birthday').val() == '' || $('#email').val() == '' || $('#phone').val() == '') {
+        alert("Hãy nhập đầy đủ thông tin");
+        infoState = true;
+    }
+    else if (checkEmail() == false) {
+        alert('Hãy nhập đúng Email(ví dụ: aaa@gmail.com)');
+        infoState = true;
+    }
+    else if ($('#birthday').val().length != 4) {
+        //$('#birthday').val('');
+        $('#birthday').focus();
+        alert('năm sinh chỉ 4 ký tự')
+        infoState = true;
     }
     else{
-      
-      current += 1;
-      
-    }
-    
-    gotoPage(current);  
-      
-  }
-  
-  function gameStart(){
-    nextBUTTON();
-    readyGame();
-  }
-  
-  //Count down function
-  function countdown(t, callback) { // t la tong so giay, tinh ra so phut va so giay
-      if (t > 0) {
-          if (isClockRun) {
-              t -= 1;
-              var m;
-              var s;
-              if (t > 60) {
-                  var m = t / 60;
-                  m = checkTime(m);
-                  var s = t % 60;
-                  s = checkTime(s);
-              } else {
-                  m = "00";
-                  s = checkTime(t);
-              }
-  
-              var txtClock = m + ":" + s;
-  
-              $("#clock").text(txtClock);
-              setTimeout(function() {
-                  countdown(t, callback)
-              }, 1000);
-              
-          } else {
-              $("#clock").text("00:00");
-          }
-  
-      } else {
-          callback();
-      }
-  
-  }
-  
-  function checkScore(s) {
-      if (s < 0) {
-          s = "" + s;
-      }else if (s < 10) {
-          s = "000" + s;
-      }else if (s < 100) {
-          s = "00" + s;
-      }else if (s < 1000) {
-          s = "0" + s;
-      }
-      return s;
-  }
-  
-  function checkTime(i) {
-      if (i < 10) {
-          i = "0" + i
-      }; // add zero in front of numbers < 10
-      return i;
-  }
-  
-  function setTitle(key) {
-    //$("#title").text(listTile[key]);
-  }
-      //Game function
-  function randomWord() {
-  
-      var listBtnGame = $(".btn-game");
-      var arrayNewBtnID = shuffle(arrayBtnID);
-      for (var i = 0; i < listBtnGame.length; i++) {
-          listBtnGame.eq(i).attr("data-id", arrayBtnID[i]);
-  
-          var mWord = listWord[arrayBtnID[i] - 1]["word"];
-          var mScore = listWord[arrayBtnID[i] - 1]["score"];
-          listBtnGame.eq(i).attr("data-score", mScore);
-  
-          listBtnGame.eq(i).empty();
-          if (mWord.indexOf("<img") > -1) {
-              listBtnGame.eq(i).append(mWord);
-          } else {
-              listBtnGame.eq(i).text(mWord);
-          }
-  
-      }
-  
-  }
-  
-  function shuffle(array) {
-      var currentIndex = array.length,
-          temporaryValue, randomIndex;
-  
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
-  
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
-  
-          // And swap it with the current element.
-          temporaryValue = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temporaryValue;
-      }
-  
-      return array;
-  }
-  
-  function initGame() {
-      mScore = 0;
-      $("#score").text("0000");
-      randomWord();
-      $(".btn-game").css("opacity", "1.0");
-  }
-  
-  function readyGame() {
-      initGame();
-      //setTitle("newgame");
-  
-      setTimeout(function() {
-          $(".btn-game").addClass("btn-touched");
-          isClockRun = true;
-          waitToStart();
-      }, 1000);
-  
-  }
-  
-  function waitToStart() {
-      //setTitle("ready");
-      countdown(waitTime, function() {
-          startGame();
-      });
-  }
-  
-  function startGame() {
-      //setTitle("start");
-      countBtn = $(".btn-game").length;
-  
-      $(".btn-game").removeClass("btn-touched");
-      isStart = true;
-      
-      countdown(playTime, function() {
-          stopGame();
-      });
-  }
-  
-  function stopGame() {
-    
-      isStart = false;
-      isClockRun = false;     
-      
-      
-      if (countBtn <= 0) {
-            if (mScore >= 200) {
-                  
-                  setTimeout(function(){
-                        gotoPage(8);
-                  },3000);
-                  
-            }else if(mScore < 200){
-                  setTimeout(function(){
-                        gotoPage(9);
-                  },3000);
+        infoState = false;
+        $.ajax({
+            type:'POST',
+            url:'ajax.php',
+            data:{
+                'action':'addPlayer',
+                'name':$('#name').val(),
+                'birthday':$('#birthday').val(),
+                'phone':$('#phone').val(),
+                'email':$('#email').val(),
+            },
+            success:function(msg){
+                $temp = JSON.parse(msg);
+                if ($temp.result == 1) {
+                    console.log("Nhập dữ liệu thành công");
+                }
+                else{
+                    console.log("Nhập dữ liệu thất bại");
+                }
             }
-      } else {
-            setTimeout(function(){
-                  gotoPage(9);
-            },3000);
-      }
-      
-      
-  }
-  
-  function resetGame() {
-      //setTitle("newgame");
-      countBtn = $(".btn-game").length;
-      
-      $(".btn-game").css("opacity", "0.0");
-      $(".btn-game").removeClass("btn-touched");
-      //setTimeout(function(){readyGame();},5000);
-  }
-  
-  function restartApp() {
-      window.location.href = '';
-  }
-  
-
-initSaveConfig();
-function initSaveConfig(){
-  if (localStorage.infoEnable == undefined) {
-    
-    localStorage.setItem('infoEnable','0')
-    
-  }
-  
-  if(localStorage.infoEnable == '0'){
-    
-  }else if(localStorage.infoEnable == '1'){
-    
-  }
-  
-}
-function toggleInfo() {
-    if(localStorage.infoEnable == '0'){
-        localStorage.setItem('infoEnable','1');
-    }else if(localStorage.infoEnable == '1'){
-        localStorage.setItem('infoEnable','0');
+        })
     }
-    
-    restartApp();
+}
+
+
+function startgame() {
+   randTitle(0, title.length -1);
+   question_quantity = title[cate].question.length;
+   addQuestion(question_quantity);
+}
+function randTitle(min,max) {
+   cate = Math.floor(Math.random()*(max-min+1)+min);
+    console.log(cate +'__'+ title[cate].category);
+   return cate;
+}
+
+
+function letCountUser() {
+  countUser = localStorage.getItem("countUser");
+  if (countUser == null) {
+    countUser == 0;
+  }
+  localStorage.setItem('countUser', ++countUser);
+}
+
+function reset() { 
+  if (countToOpen >= 2) {
+    window.location.reload();
+    countToOpen = 0;
+  }
+  countToOpen++;
+}
+
+function openHidenPanel() {
+  if (countToOpen >= 2) {
+    $(".page[page-id=8]").find("h2").text('Số người đã chơi là '+localStorage.getItem('countUser'));
+    gotoPage(8); 
+    countToOpen = 0;       
+  }
+  countToOpen++;
+  console.log(countToOpen);
+}
+
+function clearData() {
+  if (confirm('Are you sure to delete counting data?')) {
+    $(".page[page-id=8]").find("h2").text('Số người đã chơi là 0');
+    localStorage.setItem('countUser', 0);
+  }
 }
